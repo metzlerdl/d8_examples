5539edc16b70b2e7694811ecaa38474c898cba22
David Metzler <metzlerd@gmail.com>
2015-09-17 06:09:16 -0700

    Refactored field names.

---
+++ src/Controller/Page.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\form_example\Controller\Page.
+ */
+
+namespace Drupal\fapi_example\Controller;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+
+/**
+ * Simple page controller for drupal.
+ */
+class Page extends ControllerBase {
+  /**
+   * Lists the examples provided by form_example.
+   */
+  public function description() {
+    $content['intro'] = array(
+      '#markup' => '<p>' . $this->t('Form examples to demonstrate comment UI solutions using the Drupal Form API.') . '</p>',
+    );
+    $content['links'] = array(
+      '#theme' => 'item_list',
+      '#items' => array(
+        $this->l($this->t('Simple Form'), new Url('form_example.simple_form')),
+      ),
+    );
+    return $content;
+  }
+
+}
